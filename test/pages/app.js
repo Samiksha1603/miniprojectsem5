8cc1f76cddab6a1f4c42a5f31556784a9316ad94
Samiksha <2020.samiksha.wadibhasme@ves.ac.in>
2023-02-25 00:02:36 +0530

    first commit

---
+++ test/pages/app.js
@@ -0,0 +1,24 @@
+
+import { Navigation } from './Navbar';
+import { Home } from './Home';
+
+import {SSRProvider} from 'react-aria';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Card from 'react-bootstrap/Card';
+// import ListGroup from 'react-bootstrap/ListGroup';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Button from 'react-bootstrap/Button';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+// <Cardsss/>
+
+function App() {
+  return (
+    <SSRProvider>
+      <Navigation />
+    </SSRProvider>
+  );
+}
+
+export default App;
